1957890cf94164098f2b5ed3a1c6f8837815407e
deep <deeppatel234@gmail.com>
2017-08-31 10:17:34 +0530

    init master

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header.js';
+import Add from './Add.js';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header />
+            <Add />
+         </div>
+      );
+   }
+}
+
+export default App;
